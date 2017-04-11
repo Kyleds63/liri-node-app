64114c9aafb971e13b0898793bd9ea41f20e1e70
Kyleds63 <kstoner63@gmail.com>
2017-04-10 20:13:23 -0500

    Initial Setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'UZML7pCxDydXqvxPJUkGgEVcJ',
+  consumer_secret: 'J6qIghsra9Ok93XnFG2zInhC8jZNWFRBKa3mI5T2Ef5GdaGDBM',
+  access_token_key: '3022902108-nFt7yzkLIroewTcLpxHcn83tZci4nOllEZF8hE2',
+  access_token_secret: 'mgCbPEzu7IdLDs5qxa78rCGgKsPW1fcmVKNUWBIZZWcwV',
+}
---
+++ liri.js
@@ -0,0 +1,18 @@
+var keysInfo = require("./keys.js");
+
+switch(process.argv[2]){
+	case "my-tweets":
+	  var dataSum=0
+	  for(var i=0;i<dataArr.length; i++){
+	    dataSum=dataSum+parseFloat(dataArr[i]);
+      }
+      console.log(dataSum);
+	  break;
+	case "spotify-this-song":
+	  console.log("sportify");
+	  break;
+	case "movie-this":
+	  break;
+	case "do-what-it-says"
+	  break;
+}
