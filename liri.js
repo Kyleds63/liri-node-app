79336e187e3aeef6aa8c5aa8e9bdc856d2d5c0cf
kyleds63 <kstoner63@gmail.com>
2017-04-10 21:31:18 -0500

    added twitter ajax

--- liri.js
+++ liri.js
@@ -1,18 +1,24 @@
-var keysInfo = require("./keys.js");
-
 switch(process.argv[2]){
 	case "my-tweets":
-	  var dataSum=0
-	  for(var i=0;i<dataArr.length; i++){
-	    dataSum=dataSum+parseFloat(dataArr[i]);
-      }
-      console.log(dataSum);
+      var q = '%40elonmusk';
+      var result_type = 'recent';
+      var count = 20;
+
+      $.ajax({
+      	url: 'https://api.twitter.com/1.1/search/tweets.json?q='+ q +'&result_type='+result_type+'&count='+count,
+      	method: "Get"
+        
+      	})
+        .done(function(data){
+        	console.log(data)
+        });
+      
 	  break;
 	case "spotify-this-song":
 	  console.log("sportify");
 	  break;
 	case "movie-this":
 	  break;
-	case "do-what-it-says"
+	case "do-what-it-says":
 	  break;
 }
