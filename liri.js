9e9599f2a891c43ab412c3271bc8af322f6829f0
Kyleds63 <kstoner63@gmail.com>
2017-04-14 21:57:35 -0500

    Final

--- liri.js
+++ liri.js
@@ -1,24 +1,84 @@
-switch(process.argv[2]){
-	case "my-tweets":
-      var q = '%40elonmusk';
-      var result_type = 'recent';
-      var count = 20;
+var stuffINeed = require("./keys.js");
+var Twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request');
+var fs = require("fs");
 
-      $.ajax({
-      	url: 'https://api.twitter.com/1.1/search/tweets.json?q='+ q +'&result_type='+result_type+'&count='+count,
-      	method: "Get"
-        
-      	})
-        .done(function(data){
-        	console.log(data)
-        });
-      
+var client = new Twitter({
+  consumer_key: 'UZML7pCxDydXqvxPJUkGgEVcJ',
+  consumer_secret: 'J6qIghsra9Ok93XnFG2zInhC8jZNWFRBKa3mI5T2Ef5GdaGDBM',
+  access_token_key: '3022902108-nFt7yzkLIroewTcLpxHcn83tZci4nOllEZF8hE2',
+  access_token_secret: 'mgCbPEzu7IdLDs5qxa78rCGgKsPW1fcmVKNUWBIZZWcwV',
+});
+
+
+
+
+  switch(process.argv[2]){
+    case "my-tweets":
+      client.get('statuses/user_timeline', {screen_name: 'elonmusk'}, function(error, tweets, response) {
+      console.log(JSON.stringify(tweets));
+      });
+      break;
+
+    case "spotify-this-song":
+      var song;
+      var band;
+      if(process.argv[3]===""){
+    
+      }else{
+    	song = process.argv[3];
+      }
+
+      spotify.search({ type: 'track', query: song }, function(err, data) {
+        if ( err ) {
+          console.log('Error occurred: ' + err);
+        return;
+        }
+        console.log(JSON.stringify(data));
+      });
 	  break;
-	case "spotify-this-song":
-	  console.log("sportify");
-	  break;
-	case "movie-this":
-	  break;
-	case "do-what-it-says":
-	  break;
-}
+
+    case "movie-this":
+      request('http://www.omdbapi.com/?t='+process.argv[3], function (error, response, body) {
+      var movie   = JSON.parse(body);
+      });
+      var title  = movie.Title;
+      var year    = movie.Year;
+      var rating  = movie.imdbRating;
+      var country = movie.Country;
+      var lang    = movie.Language;
+      var plot    = movie.Plot;
+      var actors  = movie.Actors;
+      var rottenT = movie.Ratings[1]
+      console.log('Title: '    +title+ '\n'
+                 +'Year: '     +year +'\n'
+                 +'Rating: '   +rating+'\n'
+                 +'Country: '  +country+'\n'
+                 +'Language: ' +lang+'\n'
+                 +'Plot: '     +plot+'\n'
+                 +'Actors: '   +actors+'\n'
+                 +'Rotten Tomatoes Rating: '  +rottenT+'\n'
+                 +'Rotten Tomatoes URL: '); 
+  
+      break;
+
+    case "do-what-it-says":
+      var song;
+      fs.readFile("random.txt", "utf8", function(error, data) {
+      var dataArr = data.split(",");
+      var song =dataArr[1];
+      return song;
+      });
+
+       spotify.search({ type: 'track', query: song }, function(err, data) {
+        if ( err ) {
+          console.log('Error occurred: ' + err);
+        return;
+        }
+        console.log((typeof data));
+      });
+      break;
+  }
+
+
